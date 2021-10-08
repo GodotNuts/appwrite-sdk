09b9bb6b3f51d418fb71a199e5297fdebdd57a0b
fenix-hub <n.santilio97@gmail.com>
2021-10-08 19:08:59 +0200

    configure authentication

--- addons/supabase/plugin.gd
+++
@@ -1,9 +0,0 @@
-tool
-extends EditorPlugin
-
-
-func _enter_tree():
-    add_autoload_singleton("Supabase", "res://addons/supabase/Supabase/supabase.gd")
-
-func _exit_tree():
-    remove_autoload_singleton("Supabase")
